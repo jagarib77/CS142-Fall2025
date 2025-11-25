adba47a20971e042ca72407c10f968be373e520c
Av1dc0d3r <88227377+Av1dc0d3r@users.noreply.github.com>
2025-11-25 13:22:18 -0800

    Changed it to only storing one direction but being able to choose multiple direction

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,17 +1,16 @@
 //CS142
-//Author : Afnan Ali
+//Author : Afnan Ali & Thaknin Hor
 //Date : 11/24/2025
 
 package OceanPath;
 
 import java.util.*;
 
-
 public class Location {
-    
+
     private final int x;
     private final int y;
-    private final List<Direction> directions = new ArrayList<>();
+    private Direction direction;
     private final Random r = new Random();
 
     public Location(int x, int y){
@@ -19,29 +18,43 @@ public class Location {
         this.y = y;
     }
 
-    public int getX(){
-        return x;
-    }
-    public int getY(){
-        return y;
+    public void addDirection(Direction d) {
+        this.direction = d;
     }
 
-    public void addDirection(Direction d) {
-        if (d!= null) {
-            directions.add(d);            
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public Direction chooseRandomDirection() {
+        if (direction.equals(Direction.N)) {
+            Direction[] options = {Direction.N, Direction.NE, Direction.NW};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.E)) {
+            Direction[] options = {Direction.E, Direction.NE, Direction.SE};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.S)) {
+            Direction[] options = {Direction.S, Direction.SE, Direction.SW};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.W)) {
+            Direction[] options = {Direction.W, Direction.SW, Direction.NW};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.NE)) {
+            Direction[] options = {Direction.NE, Direction.N, Direction.E};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.SE)) {
+            Direction[] options = {Direction.SE, Direction.S, Direction.E};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.SW)) {
+            Direction[] options = {Direction.SW, Direction.S, Direction.W};
+            return options[r.nextInt(options.length)];
+        } else if (direction.equals(Direction.NW)) {
+            Direction[] options = {Direction.NW, Direction.N, Direction.W};
+            return options[r.nextInt(options.length)];
+        } else {
+            return Direction.C;
         }
     }
-
-    public void clearDirections(){
-        directions.clear();
-    }
-
-    public List<Direction> getDirections(){
-        return new ArrayList<>(directions);
-    }
-
-    public Direction chooseRandomDirection(){
-
-        return directions.get(r.nextInt(directions.size()));
-    }
 }
+    
+
