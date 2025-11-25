218f35c1cf21adb9ce5b3a1fdc17d36ca5c88626
Av1dc0d3r <88227377+Av1dc0d3r@users.noreply.github.com>
2025-11-24 20:53:14 -0800

    Stored Locations in the map

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,66 +1,73 @@
 /*
 CS&142
-Author : Thaknin Hor
-Date : 11/24/25
+Author : Thaknin Hor & Afnan Ali
+Date : 11/24/2005
  */
+
 package OceanPath;
+
 
 import java.util.*;
 import java.io.*;
 
 public class Map {
-    private Integer row;
-    private Integer col;
+
+    private int rows;
+    private int cols;
+    private Location[][] grid;
 
     public Map(String filename) throws FileNotFoundException {
-        
-        ArrayList<String> Rows = readLines(filename);
-        
-        this.row = Rows.size();
-        this.col = Rows.get(0).split(", ").length;
+        ArrayList<String> lines = readLines(filename);
 
-        for  (int r = 0; r < row; r++) {
-            String[] dirs = Rows.get(r).split(", ");
-            for (int c = 0; c < col; c++) {
-                Location loc =  new Location(c, r);
-                if (dirs[c].equals("N")) {
-                    loc.addDirection(Direction.N);
-                } else if (dirs[c].equals("NE")) {
-                    loc.addDirection(Direction.NE);
-                } else if (dirs[c].equals("E")) {
-                    loc.addDirection(Direction.E);
-                } else if (dirs[c].equals("S")) {
-                    loc.addDirection(Direction.S);
-                } else if (dirs[c].equals("SE")) {
-                    loc.addDirection(Direction.SE);
-                } else if (dirs[c].equals("W")) {
-                    loc.addDirection(Direction.W);
-                } else if (dirs[c].equals("NW")) {
-                    loc.addDirection(Direction.NW);
-                } else if (dirs[c].equals("SW")) {
-                    loc.addDirection(Direction.SW);
+        this.rows = lines.size();
+        this.cols = lines.get(0).split(", ").length;
+
+        grid = new Location[rows][cols];
+
+        for (int r = 0; r < rows - 1; r++) {
+            String[] dirs = lines.get(r).split(", ");
+
+            for (int c = 0; c < cols - 1; c++) {
+
+                Location loc = new Location(c, r);
+
+                switch (dirs[c]) {
+                    case "N": loc.addDirection(Direction.N); break;
+                    case "NE": loc.addDirection(Direction.NE); break;
+                    case "E": loc.addDirection(Direction.E); break;
+                    case "SE": loc.addDirection(Direction.SE); break;
+                    case "S": loc.addDirection(Direction.S); break;
+                    case "SW": loc.addDirection(Direction.SW); break;
+                    case "W": loc.addDirection(Direction.W); break;
+                    case "NW": loc.addDirection(Direction.NW); break;
                 }
+
+                grid[r][c] = loc;
             }
         }
-
     }
 
-    public static ArrayList<String> readLines(String fileName) throws FileNotFoundException {
-        ArrayList<String> lines = new ArrayList<String>();
+    public Location getLocation(int x,  int y) {
+        return grid[y][x];
+    }
+
+    private static ArrayList<String> readLines(String fileName) throws FileNotFoundException {
+        ArrayList<String> lines = new ArrayList<>();
         try {
             Scanner input = new Scanner(new File(fileName));
-        } 
-        catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
-        }
-        while (input.hasNextLine()) {
-            String line = input.nextLine().trim();
-            if (line.length() > 0) {
-                lines.add(line);
+            while (input.hasNextLine()) {
+                String line = input.nextLine().trim();
+                if (line.length() > 0) {
+                    lines.add(line);
+                }
             }
+
+            input.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File " + fileName + " not found.");
         }
         return lines;
+        
     }
 }
-
-
