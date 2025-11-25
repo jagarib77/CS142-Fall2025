2cf480a17917672e81761504eebf3e7fdde91a13
Av1dc0d3r <88227377+Av1dc0d3r@users.noreply.github.com>
2025-11-24 16:54:25 -0800

    Create Direction.java

---
+++ src/main/java/Direction.java
@@ -0,0 +1,31 @@
+/*  
+CS&142
+Author : Thaknin Hor
+Date : 11/24/2025
+*/
+
+public class Direction {
+    public String name;
+    public Integer dx;
+    public Integer dy;
+
+    public Direction(String name, Integer dx, Integer dy) {
+        this.name = name;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public static final Direction N = new Direction("N", 0, 1);
+    public static final Direction NE = new Direction("NE", 1, 1);
+    public static final Direction E = new Direction("E", 1, 0);
+    public static final Direction S = new Direction("S", 0, -1);
+    public static final Direction SE = new Direction("SE", 1, -1);
+    public static final Direction W = new Direction("W", -1, 0);
+    public static final Direction SW = new Direction("SW", -1, -1);
+    public static final Direction NW = new Direction("NW", -1, 1);
+
+    public String toString(){
+        return name;
+    }
+
+}
