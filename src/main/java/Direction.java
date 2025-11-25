fccc4e487e800a6c817545b18fa39c70e362b4f3
Av1dc0d3r <88227377+Av1dc0d3r@users.noreply.github.com>
2025-11-25 02:17:38 -0800

    Added ASCII Directions

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,8 +1,10 @@
-/*  
+/*
 CS&142
 Author : Thaknin Hor
-Date : 11/24/2025
-*/
+Date: 11/25/2005
+ */
+
+package OceanPath;
 
 public class Direction {
     public String name;
@@ -15,17 +17,28 @@ public class Direction {
         this.dy = dy;
     }
 
-    public static final Direction N = new Direction("N", 0, 1);
-    public static final Direction NE = new Direction("NE", 1, 1);
+    public static final Direction N = new Direction("N", 0, -1);
+    public static final Direction NE = new Direction("NE", 1, -1);
     public static final Direction E = new Direction("E", 1, 0);
-    public static final Direction S = new Direction("S", 0, -1);
-    public static final Direction SE = new Direction("SE", 1, -1);
+    public static final Direction S = new Direction("S", 0, 1);
+    public static final Direction SE = new Direction("SE", 1, 1);
     public static final Direction W = new Direction("W", -1, 0);
-    public static final Direction SW = new Direction("SW", -1, -1);
-    public static final Direction NW = new Direction("NW", -1, 1);
+    public static final Direction SW = new Direction("SW", -1, 1);
+    public static final Direction NW = new Direction("NW", -1, -1);
+    public static final Direction C = new Direction("C", 0, 0);
 
     public String toString(){
-        return name;
+        switch(name) {
+            case "N": return " ↑ ";
+            case "S": return " ↓ ";
+            case "W": return " ← ";
+            case "E": return " → ";
+            case "NE": return " ↗ ";
+            case "SE": return " ↘ ";
+            case "SW": return " ↙ ";
+            case "NW": return " ↖ ";
+            default: return name;
+        }
     }
 
 }
