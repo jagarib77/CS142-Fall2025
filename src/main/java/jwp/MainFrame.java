62a67f3f29ed008941cdb06480cd15363b980ad2
Landon <lcarothers00@gmail.com>
2025-11-30 15:59:52 -0800

    Update MainFrame and PlagueIncSimulator

--- src/main/java/jwp/MainFrame.java
+++ src/main/java/jwp/MainFrame.java
@@ -17,7 +17,29 @@ public class MainFrame extends JFrame {
 
         // NEW: use PlagueIncSimulator
         PlagueIncSimulator.registerStates(gridPanel);
-        gridPanel.setSimulator(new PlagueIncSimulator());
+
+        // create the simulator
+        PlagueIncSimulator simulator = new PlagueIncSimulator();
+
+        // OPTION 1: choose a preset disease profile
+        // Uncomment ONE of these to test a specific profile:
+
+        // simulator.setDiseaseProfile(PlagueIncSimulator.PROFILE_MILD_FLU);
+        // simulator.setDiseaseProfile(PlagueIncSimulator.PROFILE_DEADLY_VIRUS);
+        simulator.setDiseaseProfile(PlagueIncSimulator.PROFILE_ZOMBIE_PLAGUE);
+
+        // OPTION 2: or define a custom disease (comment out the preset if you use this)
+        // simulator.setCustomDisease(
+        //         "Custom Plague",
+        //         0.30, // infection probability
+        //         0.15, // death probability
+        //         0.70, // movement probability
+        //         8,    // min infection duration (ticks)
+        //         25    // max infection duration (ticks)
+        // );
+
+        // attach the simulator to the grid
+        gridPanel.setSimulator(simulator);
 
         setContentPane(gridPanel); 
         setResizable(false);       
