d62b4b2d06387900ed57fd64e50abc9e2db5f81c
Tsubasa <tsubasa09240525@gmail.com>
2025-12-02 03:28:06 -0800

    Refactor Entity class to be abstract with new methods

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,78 +1,23 @@
-import java.awt.Color;
+import java.awt.*;
 
-public class Entity {
-    private int row;
-    private int col;
-    private boolean infected;
-    private boolean immune;
+public abstract class Entity {
+    //checks all possible sorrounding squares with
+    //each direction.
+    protected static final int[][] DIRECTIONS = {
+            {-1, 0},  {1, 0},   {0, -1}, {0, 1},
+            {-1, 1},  {-1, -1}, {1, 1},  {1, -1}
+    };
 
-    public Entity(int row, int col) {
-        this.row = row;
-        this.col = col;
-        this.infected = false;
-        this.immune = false;
-    }
+    //@override infect or defend depending on object
+    public abstract void performAction(Entity[][] grid, double effectRate, int row, int column);
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+    //each class gets its own unique color
+    public abstract Color getColor();
 
-    public void setCol(int col) {
-        this.col = col;
-    }
 
-    public void setInfected(boolean infected) {
-        this.infected = infected;
-    }
+    // default = false, subclasses override what they are. Only zombie classes need to override
+    public boolean isHuman() { return true; }
 
-    public void setImmune(boolean immune) {
-        this.immune = immune;
-    }
-
-    public void move(Entity[][] grid) {
-    }
-
-    public void interact(Entity[][] grid) {
-    }
-
-    public Color getColor() {
-        return Color.WHITE;
-    }
-
-    public boolean isInfected() {
-        return infected;
-    }
-
-    public boolean isImmune() {
-        return immune;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
-    public Entity[][] getNeighbors(Entity[][] grid) {
-        Entity[][] neighbors = new Entity[3][3];
-        for (int r = -1; r <= 1; r++) {
-            for (int c = -1; c <= 1; c++) {
-                int newRow = row + r;
-                int newCol = col + c;
-                if (isInBounds(newRow, newCol, grid)) {
-                    neighbors[r + 1][c + 1] = grid[newRow][newCol];
-                } else {
-                    neighbors[r + 1][c + 1] = null;
-                }
-            }
-        }
-        return neighbors;
-    }
-
-    public boolean isInBounds(int r, int c, Entity[][] grid) {
-        return r >= 0 && r < grid.length && c >= 0 && c < grid[0].length;
-    }
-
+    //only chosen one is immune
+    public boolean isImmune() { return false;}
 }
