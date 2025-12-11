7829a5cc3d670d9ed5f376a866cf15271c51c793
Landon <lcarothers00@gmail.com>
2025-12-11 10:09:56 -0800

    Add files via upload

--- DeadHost.java
+++ DeadHost.java
@@ -1,9 +1,18 @@
 package jwp;
-
-/**
- * non-moving, non-infectious corpse marker
- */
-
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Class         DeadHost
+ * File          DeadHost.java
+ * Description   Represents a host that died from the disease. Dead hosts do not
+ *               move, cannot be infected again, and do not contribute to further
+ *               spread, but they are counted in statistics and help drive cure
+ *               progress by increasing the visible severity of the outbreak.
+ * @author       <i>Landon Carothers</i>
+ * Environment   Java SE 24, NetBeans IDE 25, Windows 10
+ * Date          11/25/2025
+ * @version      1.0
+ * @see          HostAgent
+ * @see          PlagueIncSimulator
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 public class DeadHost extends HostAgent {
 
     public DeadHost(int row, int col) {
